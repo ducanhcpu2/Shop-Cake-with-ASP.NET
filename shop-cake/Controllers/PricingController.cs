0e5f6fcc4ed596388864135c871afb2250bc45bd
ducanhcpu2 <ducanhprogramer98@gmail.com>
2019-04-07 11:49:37 +0700

    first_commit

---
+++ shop-cake/Controllers/PricingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace shop_cake.Controllers
+{
+    public class PricingController : Controller
+    {
+        // GET: Pricing
+        public ActionResult Pricing()
+        {
+            return View();
+        }
+    }
+}
---
+++ shop-cake/Controllers/ProductTypeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace shop_cake.Controllers
+{
+    public class ProductTypeController : Controller
+    {
+        // GET: ProductType
+        public ActionResult ProductType()
+        {
+            return View();
+        }
+    }
+}
