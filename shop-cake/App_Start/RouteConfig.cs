ec108c7070e78ac16a2920e8b61a520e634de8c7
LeO0999 <longkyle109@gmail.com>
2019-04-15 14:00:24 +0700

    them duong dan trong IndexPage chuc nang san pham va danh muc

--- shop-cake/App_Start/RouteConfig.cs
+++ shop-cake/App_Start/RouteConfig.cs
@@ -14,6 +14,16 @@ namespace shop_cake
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "AdminProduct",
+                url: "AdminProduct/{action}/{id}",
+                defaults: new { controller = "AdminProduct", action = "Index", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+                name: "AdminCategory",
+                url: "AdminCategory/{action}/{id}",
+                defaults: new { controller = "AdminCategory", action = "Index", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
