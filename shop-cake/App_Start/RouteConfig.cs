248f4620dea3612a6b82026978b473318a6869a7
LeO0999 <longkyle109@gmail.com>
2019-04-17 23:32:06 +0700

    Login SignUp nguoi dung

--- shop-cake/App_Start/RouteConfig.cs
+++ shop-cake/App_Start/RouteConfig.cs
@@ -29,6 +29,17 @@ namespace shop_cake
                 defaults: new { controller = "AdminCategory", action = "Index", id = UrlParameter.Optional }
             );
             routes.MapRoute(
+               name: "SignUp",
+               url: "SignUp/{action}/{id}",
+               defaults: new { controller = "SignUp", action = "SignUp", id = UrlParameter.Optional }
+           );
+            routes.MapRoute(
+               name: "Login",
+               url: "Login/{action}/{id}",
+               defaults: new { controller = "Login", action = "Login", id = UrlParameter.Optional }
+           );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
