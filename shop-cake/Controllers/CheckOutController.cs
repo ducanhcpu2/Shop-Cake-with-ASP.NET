0cee98c9fa7d0cfb3e9eaa759817a8e68bb9fdb4
ducanhcpu2 <ducanhprogramer98@gmail.com>
2019-05-09 23:57:18 +0700

    done_cart

--- shop-cake/Controllers/CheckOutController.cs
+++ shop-cake/Controllers/CheckOutController.cs
@@ -1,6 +1,7 @@
 ﻿using shop_cake.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,7 +17,7 @@ namespace shop_cake.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult CheckOut(customer cus )
+        public ActionResult CheckOut(customer cus, string payment_method )
         {
             var cart = (ShoppingCart)Session["cart"];
             if (cart == null)
@@ -26,49 +27,49 @@ namespace shop_cake.Controllers
 
             if (ModelState.IsValid)
             {
-                db.customers.Add(new customer
+                var obj = db.customers.FirstOrDefault(x => x.id_customer == cus.id_customer);
+                if (cus.name != null) obj.name = cus.name;
+
+                if (cus.email != null) obj.email = cus.email;
+                if (cus.gender != null) obj.gender = cus.gender;
+                if (cus.phone_number != null)  obj.phone_number = cus.phone_number;
+                if (cus.address != null)  obj.address = cus.address;
+                obj.created_at = System.DateTime.Now;
+                obj.updated_at = System.DateTime.Now;
+                db.Entry(obj).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+            }
+
+            db.bills.Add(new bill
+            {
+                date_order = System.DateTime.Now,
+                payment = payment_method,
+                total = cart.TotalPrice(),
+                note = cus.note,
+                created_at = System.DateTime.Now,
+                updated_at = System.DateTime.Now,
+                id_customer = cus.id_customer,
+            });
+
+            db.SaveChanges();
+
+            var lastIdBill = db.bills.Max(x => x.id_bill);
+            foreach (var i in cart.Item)
+            {
+                db.bill_detail.Add(new bill_detail
                 {
-                    name = cus.name,
-                    email = cus.email,
-                    gender = cus.gender,
-                    phone_number = cus.phone_number,
-                    address = cus.address,
+                    id_bill = lastIdBill,
+                    id_product = i.id,
+                    quantity = i.quantity,
+                    unit_price = i.promotion_price > 0 ? i.promotion_price : i.unit_price,
+                    created_at = System.DateTime.Now,
                     updated_at = System.DateTime.Now
                 });
-
-                db.bills.Add(new bill
-                {
-                    date_order = System.DateTime.Now,
-                    total = cart.TotalPrice(),
-                    note = cus.note,
-                    created_at = System.DateTime.Now,
-                    updated_at = System.DateTime.Now,
-                    id_customer = cus.id_customer,
-
-
-                });
-
-                db.SaveChanges();
-
-                var Current_Bill = db.bills.LastOrDefault(s => s.id_customer == cus.id_customer);
-                foreach (var i in cart.Item)
-                {
-                    db.bill_detail.Add(new bill_detail
-                    {
-                        id_bill = Current_Bill.id_bill,
-                        id_product = i.id,
-                        quantity = i.quantity,
-                        unit_price = i.promotion_price > 0 ? i.promotion_price : i.unit_price,
-                        created_at = System.DateTime.Now,
-                        updated_at = System.DateTime.Now
-                    });
-                }
-
-                db.SaveChanges();
-
-                return RedirectToAction("Index","Home");
             }
-            return Redirect(Request.UrlReferrer.ToString());
+
+            db.SaveChanges();
+            Session["cart"] = null;
+             return RedirectToAction("Index","Home");
         }
     }
 }
