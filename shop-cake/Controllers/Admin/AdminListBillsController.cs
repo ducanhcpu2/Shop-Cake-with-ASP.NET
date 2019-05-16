d85a31a0cbefbc9fa041b8919d0a4b0487e361ab
LeO0999 <longkyle109@gmail.com>
2019-05-16 20:33:52 +0700

    logoutadmin

---
+++ shop-cake/Controllers/Admin/AdminListBillsController.cs
@@ -0,0 +1,20 @@
+﻿using shop_cake.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace shop_cake.Controllers.Admin
+{
+    public class AdminListBillsController : Controller
+    {
+        // GET: AdminListBills
+        private shop_cakeEntities1 db = new shop_cakeEntities1();
+        public ActionResult Index()
+        {
+            ViewBag.Bills = db.bills;
+            return View();
+        }
+    }
+}
