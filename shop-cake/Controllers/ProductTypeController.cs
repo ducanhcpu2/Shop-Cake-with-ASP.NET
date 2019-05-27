2cfd63eeef6627864bef0f9f36147aa2cdcb58d1
ducanhcpu2 <ducanhprogramer98@gmail.com>
2019-05-27 10:30:50 +0700

    fix_size_column_in_admin

--- shop-cake/Controllers/ProductTypeController.cs
+++ shop-cake/Controllers/ProductTypeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PagedList;
+using shop_cake.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,55 @@ namespace shop_cake.Controllers
 {
     public class ProductTypeController : Controller
     {
+        shop_cakeEntities1 db = new shop_cakeEntities1();
         // GET: ProductType
-        public ActionResult ProductType()
+        public ActionResult ProductType(int? a)
         {
+            ViewBag.IDProductType = a;
             return View();
         }
+
+        public PartialViewResult ListProductType()
+        {
+
+            return PartialView("ListProductType", db.product_types);
+        }
+        
+        public PartialViewResult ListProduct(int? a, string currentFilter, string searchString, int? Page_No)
+        {
+            if (a == null)
+            {
+                a = 1;
+            }
+            var product = db.products.Where(s => s.id_product_type==a);
+            //start search
+            if (searchString != null)
+            {
+                Page_No = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+            var products = from s in product
+                           select s;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                products = products.Where(s => s.name.Contains(searchString));
+
+            }
+            //end search
+
+            //paginate
+            ViewBag.id_product_type = a;
+            ViewBag.totalItem = product.Count();
+            int Size_Of_Page = 4;
+            int No_Of_Page = (Page_No ?? 1);
+            return PartialView("ListProduct", products.OrderBy(i => i.id_product).ToPagedList(No_Of_Page, Size_Of_Page));
+        }
+        
     }
 }
